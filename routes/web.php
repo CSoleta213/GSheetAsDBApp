1c9dbfec90d4bb2f0680d5c2ac6971ed75aef255
CSoleta213 <72241444+CSoleta213@users.noreply.github.com>
2022-06-08 17:15:32 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('record');
+    return view('welcome');
 });
 
 Route::get('get', [SheetdbController::class, 'get']);
